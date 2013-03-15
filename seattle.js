b553651f32980e6db6bff86b20b8988a2e2d33fd
Yuriy Dybskiy <yuriy@dybskiy.com>
2013-03-14 23:47:47 -0700

    Remove comments and fix naming

--- seattle.js
+++ seattle.js
@@ -1,7 +1,5 @@
-Directory = new Meteor.Collection("thumbnails");
+Thumbnails = new Meteor.Collection("thumbnails");
 
 var userEmail = function (user) {
-  // if (user.profile && user.profile.name)
-  //   return user.profile.name;
   return user.emails[0].address;
 };
